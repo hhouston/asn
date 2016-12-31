6fa09b8b62e6c0df7f44b7b47260172c23c601a2
David Hu <davidhu314@gmail.com>
2016-12-31 14:59:19 -0800

    add session ajax requests

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,22 @@
+export const login = user => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/session',
+    data: { user: user }
+  });
+};
+
+export const logout = () => {
+  return $.ajax({
+    method: 'DELETE',
+    url: 'api/session'
+  });
+};
+
+export const signup = user => {
+  return $.ajax({
+    method: 'CREATE',
+    url: 'api/users',
+    data: { user: user }
+  });
+};
