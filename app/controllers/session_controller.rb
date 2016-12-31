6a3b2e5edb49b8809f57a42067d5d64d28d1e09c
David Hu <davidhu314@gmail.com>
2016-12-31 14:33:51 -0800

    add sessions and users controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,19 @@
 class SessionController < ApplicationController
+  def create
+    @user = User.find_by_credentials(session_params[:username],
+                                     session_params[:password])
+
+    if @user
+      login(@user)
+      render json: ["logged in"]
+    else
+      render json: ["Invalid credentials"], status: 401
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :password)
+  end
 end
