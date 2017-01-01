996a0353633659741e58b3119131c270f1c07a49
hhouston <hmhouston7@gmail.com>
2016-12-31 17:50:49 -0800

    adds instagram component to header.jsx

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import WelcomeContainer from './welcome/welcome_container';
+import Insta from './insta.jsx';
 
 const Header = ({ children }) => (
   <div>
+
     <WelcomeContainer />
+    <Insta />
     { children }
   </div>
 );
